4c2b9e65b32e4f00d82ebd45f7b7c69816fa494b
ManonVillanueva <g.manon14@gmail.com>
2024-08-06 12:04:39 +0200

    second part

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-// --- > OPEN & CREATE THE FORM
-const controle = (e) => {
+// => ADD TO LOCAL STORAGE
+const addLocalStorage = () => {
+  // => Récupérer les données qui sont dans le local Storage ayant la key ("notes")
+  let getNotes = localStorage.getItem("notes");
+  // => Vérifier si il y a bien des données si oui .. sinon j'initialise un tableau vide
+  let notesArray = getNotes ? JSON.parse(getNotes) : [];
+
+  let note = {
+    title: addTitle.value,
+    description: textarea.value,
+    date: dueDate.value,
+  };
+
+  notesArray.push(note);
+  const noteString = JSON.stringify(notesArray);
+
+  localStorage.setItem("notes", noteString);
+};
+
+const getStorage = () => {
+  let dataNotes = localStorage.getItem("notes");
+  let coucou = dataNotes ? JSON.parse(dataNotes) : [];
+  coucou.forEach((data) => {
+    toDoContainer.innerHTML += `<div id="toDoList">
+    <div>
+      <h2>${data.title}</h2>
+      <p>${data.description}</p>
+      <p>${data.date}</p>
+    </div>
+    <div id="iconPart">
+    <i class="fa-solid fa-pencil"></i>
+    <i class="fa-solid fa-trash"></i>
+    </div>
+    </div>`;
+  });
+};
+
+const controle = () => {
   if (addTitle.value === "" || dueDate.value === "") {
-    alert("Veuillez entrer un titre et une date limite");
+    alert("Veuillez entrer un titre");
+    e.preventDefault();
   } else {
-    const titleValue = addTitle.value;
-    const textareaValue = textarea.value;
-    const dateValue = dueDate.value;
-    console.log(textareaValue);
+    addLocalStorage();
   }
 };
 
@@ -30,9 +64,14 @@ addNote.addEventListener("click", () => {
     e.preventDefault();
     controle();
     formContainer.innerHTML = "";
+    location.reload();
   });
 
   returnBtn.addEventListener("click", () => {
     formContainer.innerHTML = "";
   });
 });
+
+window.onload = () => {
+  getStorage();
+};
